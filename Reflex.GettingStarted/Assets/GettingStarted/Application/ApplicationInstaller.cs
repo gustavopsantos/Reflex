b6d8e2843fbd73c4fa39f9d248bc45e90376c244
Gustavo Santos <gustappsantos@gmail.com>
2021-09-01 18:49:09 -0300

    Adds shared project making easier to work with

--- Reflex.GettingStarted/Assets/GettingStarted/Application/ApplicationInstaller.cs
+++ Reflex.GettingStarted/Assets/GettingStarted/Application/ApplicationInstaller.cs
@@ -6,8 +6,8 @@ public class ApplicationInstaller : MonoInstaller
     public PickupSoundEffect PickupSoundEffect;
     public override void InstallBindings(Container container)
     {
-        container.Bind<IResetGame>().To<ResetGame>().AsSingleton();
-        container.Bind<IGetPlayerInput>().To<GetPlayerInput>().AsSingleton();
+        container.Bind<IResetGame>().To<ResetGame>().AsLazySingleton();
+        container.Bind<IGetPlayerInput>().To<GetPlayerInput>().AsLazySingleton();
         container.BindSingleton(PickupSoundEffect);
     }
 }
--- Reflex.GettingStarted/Assets/GettingStarted/Infrastructure/InfrastructureInstaller.cs
+++ Reflex.GettingStarted/Assets/GettingStarted/Infrastructure/InfrastructureInstaller.cs
@@ -9,6 +9,6 @@ public class InfrastructureInstaller : MonoInstaller
     public override void InstallBindings(Container container)
     {
         container.BindSingleton<IGameSettings>(_scriptableObjectGameSettings);
-        container.Bind<ICollectableRegistry>().To<CollectableRegistry>().AsSingleton();
+        container.Bind<ICollectableRegistry>().To<CollectableRegistry>().AsLazySingleton();
     }
 }
