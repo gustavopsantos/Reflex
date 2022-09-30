d0daeb27a4b9bc82c2153d0855970cb73c01ac96
Gustavo Santos <gustappsantos@gmail.com>
2022-09-30 07:14:19 -0300

    Adds MeansImplicitUseAttribute to InjectAttribute
    Stops annoying rider messages regarding fields neber being assigned

--- Assets/Reflex/Scripts/Attributes/InjectAttribute.cs
+++ Assets/Reflex/Scripts/Attributes/InjectAttribute.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace Reflex.Scripts.Attributes
 {
+    [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
     public class InjectAttribute : Attribute { }
 }
