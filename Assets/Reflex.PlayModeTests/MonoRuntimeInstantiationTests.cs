51766c05dc218fa36a08323e15272244a70c844e
Gustavo Santos <gustappsantos@gmail.com>
2022-10-24 12:07:44 -0300

    Adds ExecutionOrderOfRuntimeInstantiatedMonoBehaviours_ShouldBe_InjectAwakeStart test

---
+++ Assets/Reflex.PlayModeTests/MonoRuntimeInstantiationTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using Reflex.Scripts.Attributes;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Reflex.PlayModeTests
+{
+    public class MonoRuntimeInstantiationTests
+    {
+        private class MonoEventHook : MonoBehaviour
+        {
+            public readonly List<string> ExecutionOrder = new List<string>();
+
+            [Inject]
+            private void Inject()
+            {
+                ExecutionOrder.Add(nameof(Inject));
+            }
+
+            private void Awake()
+            {
+                ExecutionOrder.Add(nameof(Awake));
+            }
+
+            private void Start()
+            {
+                ExecutionOrder.Add(nameof(Start));
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ExecutionOrderOfRuntimeInstantiatedMonoBehaviours_ShouldBe_InjectAwakeStart()
+        {
+            using (var container = new Container(string.Empty))
+            {
+                var prefab = new GameObject("Prefab").AddComponent<MonoEventHook>();
+                var instance = container.Instantiate(prefab);
+                yield return new WaitForEndOfFrame();
+                string.Join(",", instance.ExecutionOrder).Should().Be("Inject,Awake,Start");
+            }
+        }
+    }
+}
