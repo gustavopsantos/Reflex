4f639e77aa88a5bcc7198c4a7fec714593354712
Gustavo Santos <gustappsantos@gmail.com>
2023-05-22 22:05:01 -0300

    Adds ActivatorFactories test coverage

---
+++ Assets/Reflex.Tests/ActivatorFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Reflex.Reflectors;
+
+namespace Reflex.Tests
+{
+    public class ActivatorFactoryTests
+    {
+        [Test]
+        [TestCase(typeof(MonoActivatorFactory))]
+        [TestCase(typeof(IL2CPPActivatorFactory))]
+        public void CanActivate_ValueType(Type activatorFactoryType)
+        {
+            var activatorFactory = (IActivatorFactory) Activator.CreateInstance(activatorFactoryType);
+            var activator = activatorFactory.GenerateDefaultActivator(typeof(int));
+            var number = (int) activator.Invoke(null);
+            number.Should().Be(default(int));
+        }
+        
+        [Test]
+        [TestCase(typeof(MonoActivatorFactory))]
+        [TestCase(typeof(IL2CPPActivatorFactory))]
+        public void CanActivate_ReferenceType(Type activatorFactoryType)
+        {
+            var activatorFactory = (IActivatorFactory) Activator.CreateInstance(activatorFactoryType);
+            var activator = activatorFactory.GenerateActivator(typeof(string), typeof(string).GetConstructor(new[] {typeof(char[])}), new[] {typeof(char[])});
+            var complex = (string) activator.Invoke(Array.Empty<char>());
+            complex.Should().NotBeNull();
+            complex.Should().Be(string.Empty);
+        }
+    }
+}
