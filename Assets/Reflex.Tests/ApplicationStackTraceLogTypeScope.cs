26c8c584a43c7d600f3f35aa3d3a2dfbcda68d1b
Gustavo Santos <gustappsantos@gmail.com>
2023-09-12 10:51:30 -0300

    add test reporting

---
+++ Assets/Reflex.Tests/ApplicationStackTraceLogTypeScope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Reflex.Tests
+{
+    public class ApplicationStackTraceLogTypeScope : IDisposable
+    {
+        private readonly LogType _logType;
+        private readonly StackTraceLogType _previousStackTraceLogType;
+        
+        public ApplicationStackTraceLogTypeScope(LogType logType, StackTraceLogType stackTraceLogType)
+        {
+            _logType = logType;
+            _previousStackTraceLogType = Application.GetStackTraceLogType(logType);
+            Application.SetStackTraceLogType(logType, stackTraceLogType);
+        }
+        
+        public void Dispose()
+        {
+            Application.SetStackTraceLogType(_logType, _previousStackTraceLogType);
+        }
+    }
+}
---
+++ Assets/Reflex.Tests/TestRunnerExtensions.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+namespace Reflex.Tests
+{
+    [SetUpFixture]
+    public class TestRunnerExtensions : ICallbacks
+    {
+        private readonly List<ITestResultAdaptor> _results = new();
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var testRunnerApi = ScriptableObject.CreateInstance<TestRunnerApi>();
+            testRunnerApi.RegisterCallbacks(this);
+        }
+
+        void ICallbacks.RunStarted(ITestAdaptor testsToRun)
+        {
+            _results.Clear();
+        }
+
+        void ICallbacks.RunFinished(ITestResultAdaptor result)
+        {
+            using (new ApplicationStackTraceLogTypeScope(LogType.Log, StackTraceLogType.None))
+            {
+                ReportStatus(TestStatus.Passed);
+                ReportStatus(TestStatus.Failed);
+                ReportStatus(TestStatus.Skipped);
+                ReportStatus(TestStatus.Inconclusive);
+            }
+        }
+
+        void ICallbacks.TestStarted(ITestAdaptor test)
+        {
+        }
+
+        void ICallbacks.TestFinished(ITestResultAdaptor result)
+        {
+            if (!result.Test.IsSuite && !result.Test.IsTestAssembly)
+            {
+                _results.Add(result);
+            }
+        }
+
+        private void ReportStatus(TestStatus status)
+        {
+            var report = _results
+                .Where(r => r.TestStatus == status)
+                .Select(r => r.Name)
+                .OrderBy(r => r.Length)
+                .ToList();
+
+            report.Insert(0, $"[{status.ToString().ToUpper()} {report.Count}/{_results.Count}]");
+
+            Debug.Log(string.Join(Environment.NewLine, report));
+        }
+    }
+}
