d1a3fab66a73c840211a8b63d44303907b0165db
Gustavo Santos <gustappsantos@gmail.com>
2024-03-19 11:24:23 -0300

    Move Tests to EditModeTests specific assembly

--- Assets/Reflex/AssemblyInfo.cs
+++ Assets/Reflex/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("Reflex.Editor")]
-[assembly:InternalsVisibleTo("Reflex.Tests")]
+[assembly:InternalsVisibleTo("Reflex.EditModeTests")]
 [assembly:InternalsVisibleTo("Reflex.IL2CPP.Tests")]
 [assembly:InternalsVisibleTo("Reflex.Weaving")]
