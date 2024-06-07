5549331110d9f2239d5bf2a2f988575f313204a1
Gustavo Santos <gustappsantos@gmail.com>
2024-06-06 22:10:10 -0300

    AutoSceneInjectComponent improvements

--- Assets/Reflex/Components/GameObjectSelfInjector.cs
+++ Assets/Reflex/Components/GameObjectSelfInjector.cs
@@ -1,39 +1,40 @@
+using System;
 using Reflex.Extensions;
 using Reflex.Injectors;
 using UnityEngine;
 
 namespace Reflex.Components
 {
-    [DefaultExecutionOrder(int.MinValue + 1)]
-    public sealed class AutoSceneInjectComponent : MonoBehaviour
+    [DefaultExecutionOrder(int.MinValue + 1000)]
+    internal sealed class GameObjectSelfInjector : MonoBehaviour
     {
-        [SerializeField] private InjectType _injectType = InjectType.Object;
-        
+        [SerializeField] private InjectionStrategy _injectionStrategy = InjectionStrategy.Recursive;
+
         private void Awake()
         {
             var sceneContainer = gameObject.scene.GetSceneContainer();
 
-            switch (_injectType)
+            switch (_injectionStrategy)
             {
-                case InjectType.Single:
+                case InjectionStrategy.Single:
                     GameObjectInjector.InjectSingle(gameObject, sceneContainer);
                     break;
-                case InjectType.Object:
+                case InjectionStrategy.Object:
                     GameObjectInjector.InjectObject(gameObject, sceneContainer);
                     break;
-                case InjectType.Recursive:
+                case InjectionStrategy.Recursive:
                     GameObjectInjector.InjectRecursive(gameObject, sceneContainer);
                     break;
+                default:
+                    throw new ArgumentOutOfRangeException(_injectionStrategy.ToString());
             }
-       }
+        }
 
-        private enum InjectType
+        private enum InjectionStrategy
         {
             Single,
             Object,
             Recursive
         }
     }
-
-    
 }
