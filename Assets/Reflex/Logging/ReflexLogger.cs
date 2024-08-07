8217777f0ef896da5661a848427c28ca2c516a5e
Gustavo Santos <gustappsantos@gmail.com>
2024-08-06 23:00:29 -0300

    Simplifies ReflexLogger

--- Assets/Reflex/Logging/ReflexLogger.cs
+++ Assets/Reflex/Logging/ReflexLogger.cs
@@ -1,6 +1,5 @@
 using System;
 using Reflex.Configuration;
-using Reflex.Utilities;
 using UnityEngine;
 
 namespace Reflex.Logging
@@ -11,11 +10,13 @@ namespace Reflex.Logging
 
         static ReflexLogger()
         {
-            _logLevel = ResourcesUtilities.TryLoad<ReflexSettings>(nameof(ReflexSettings), out var reflexSettings)
+            var reflexSettings = Resources.Load<ReflexSettings>(nameof(ReflexSettings));
+
+            _logLevel = reflexSettings != null
                 ? reflexSettings.LogLevel
                 : LogLevel.Info;
             
-            ReportLogLevel();
+            Log($"Reflex LogLevel set to {_logLevel}", LogLevel.Info);
         }
 
         public static void UpdateLogLevel(LogLevel logLevel)
@@ -42,11 +43,6 @@ namespace Reflex.Logging
                 case LogLevel.Error: Debug.LogError(message, context); break;
                 default: throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
             }
-        }
-
-        private static void ReportLogLevel()
-        {
-            Log($"Reflex LogLevel set to {_logLevel}", LogLevel.Info);
         }
     }
 }
