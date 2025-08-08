a33164c8945c157fcd00f85b321c960c7acf4208
Moe-Baker <moe4baker@gmail.com>
2025-08-08 19:10:06 +0300

    Implement Injector Source Generator

--- Reflex.Generator/Reflex.Generator.Injector/InjectorSourceGenerator.cs
+++ Reflex.Generator/Reflex.Generator.Injector/InjectorSourceGenerator.cs
@@ -1,9 +1,418 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+
+using System.Collections.Generic;
 
 namespace Reflex.Generator.Injector
 {
-    public class InjectorSourceGenerator
+    [Generator]
+    public class InjectorSourceGenerator : ISourceGenerator
     {
+        public static class Constants
+        {
+            public static readonly string Namespace = "Reflex";
 
+            public static readonly string Container = $"{Namespace}.Core.{nameof(Container)}";
+            public static readonly string InjectAttribute = $"{Namespace}.Attributes.{nameof(InjectAttribute)}";
+            public static readonly string IAttributeInjectionContract = $"{Namespace}.Injectors.{nameof(IAttributeInjectionContract)}";
+            public static readonly string SourceGeneratorInjectableAttribute = $"{Namespace}.Attributes.{nameof(SourceGeneratorInjectableAttribute)}";
+        }
+
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var waypoints = new WaypointsData(context.Compilation);
+            var contractors = InjectionVisitor.Collect(waypoints, context.Compilation.GlobalNamespace);
+
+            var codeBuilder = new CodeStringBuilder();
+
+            var writtenContractCount = 0;
+
+            foreach (var contractor in contractors)
+            {
+                if (GenerateContractor(context, waypoints, codeBuilder, contractor))
+                    writtenContractCount += 1;
+            }
+
+            if (writtenContractCount == 0)
+                return;
+
+            WriteSourceFile(context, codeBuilder);
+        }
+
+        bool GenerateContractor(GeneratorExecutionContext context, WaypointsData waypoints, CodeStringBuilder codeBuilder, INamedTypeSymbol contractor)
+        {
+            if (ValidateContractorContainerChain(context, contractor) is false)
+                return false;
+
+            var cache = CollectContractorMembers(context, waypoints, contractor);
+
+            //Start Write Hierarchy
+            for (int i = 0; i < cache.Hierarchy.Count; i++)
+            {
+                var item = cache.Hierarchy[i];
+
+                if (item.IsNamespace)
+                    codeBuilder.Write("namespace ");
+                else
+                    codeBuilder.Write("partial class ");
+
+                codeBuilder.Write(item.Name);
+
+                //Last Item, Implement Interface
+                if (i == cache.Hierarchy.Count - 1)
+                {
+                    codeBuilder.Write(" : ");
+                    codeBuilder.Write(waypoints.IAttributeInjectionContract);
+                }
+
+                codeBuilder.StartBlock();
+            }
+
+            //Write Fields
+            {
+                StartWriteInterfaceImplementation(codeBuilder, "InjectFields");
+
+                if (cache.Fields.Count is 0)
+                {
+                    codeBuilder.Write(" {}");
+                    codeBuilder.Newline();
+                }
+                else
+                {
+                    using (codeBuilder.CodeBlock())
+                    {
+                        foreach (var field in cache.Fields)
+                        {
+                            codeBuilder.Write(field.Name);
+                            codeBuilder.Write(" = ");
+                            WriteContainerResolutionCall(codeBuilder, field.Type);
+                            codeBuilder.EndLine();
+                        }
+                    }
+                }
+            }
+
+            codeBuilder.Newline();
+
+            //Write Properties
+            {
+                StartWriteInterfaceImplementation(codeBuilder, "InjectProperties");
+
+                if (cache.Properties.Count is 0)
+                {
+                    codeBuilder.Write(" {}");
+                    codeBuilder.Newline();
+                }
+                else
+                {
+                    using (codeBuilder.CodeBlock())
+                    {
+                        foreach (var property in cache.Properties)
+                        {
+                            codeBuilder.Write(property.Name);
+                            codeBuilder.Write(" = ");
+                            WriteContainerResolutionCall(codeBuilder, property.Type);
+                            codeBuilder.EndLine();
+                        }
+                    }
+                }
+            }
+
+            codeBuilder.Newline();
+
+            //Write Methods
+            {
+                StartWriteInterfaceImplementation(codeBuilder, "InjectMethods");
+
+                if (cache.Methods.Count is 0)
+                {
+                    codeBuilder.Write(" {}");
+                    codeBuilder.Newline();
+                }
+                else
+                {
+                    using (codeBuilder.CodeBlock())
+                    {
+                        foreach (var method in cache.Methods)
+                        {
+                            var arguments = method.Parameters;
+
+                            codeBuilder.Write(method.Name);
+
+                            using (codeBuilder.Parameters())
+                            {
+                                for (int i = 0; i < arguments.Length; i++)
+                                {
+                                    WriteContainerResolutionCall(codeBuilder, arguments[i].Type);
+
+                                    if (i != arguments.Length - 1)
+                                        codeBuilder.Write(", ");
+                                }
+                            }
+
+                            codeBuilder.EndLine();
+                        }
+                    }
+                }
+            }
+
+            //End Write Hierarchy
+            for (int i = 0; i < cache.Hierarchy.Count; i++)
+                codeBuilder.EndBlock();
+
+            return true;
+        }
+
+        static WaypointsData.CacheData CollectContractorMembers(GeneratorExecutionContext context, WaypointsData waypoints, INamedTypeSymbol contractor)
+        {
+            var cache = waypoints.GetCache();
+
+            var members = contractor.GetMembers();
+
+            //Collect Hierarchy Containers
+            {
+                cache.Hierarchy.AddRange(contractor.IterateNamespaceOrTypeContainingChain());
+                cache.Hierarchy.Reverse();
+            }
+
+            //Collect Injectable Members
+            foreach (var member in members)
+            {
+                if (member is IFieldSymbol field)
+                {
+                    if (field.HasAttribute(waypoints.InjectAttribute) is false)
+                        continue;
+
+                    if (field.IsReadOnly)
+                    {
+                        context.ReportDiagnostic(LocalDiagnostics.InjectMemberMustNotBeReadonly.Create(field));
+                        continue;
+                    }
+
+                    cache.Fields.Add(field);
+                }
+
+                if (member is IPropertySymbol property)
+                {
+                    if (member.HasAttribute(waypoints.InjectAttribute) is false)
+                        continue;
+
+                    if (property.SetMethod == null)
+                    {
+                        context.ReportDiagnostic(LocalDiagnostics.InjectPropertiesMustBeWritable.Create(property));
+                        continue;
+                    }
+
+                    cache.Properties.Add(property);
+                }
+
+                if (member is IMethodSymbol method)
+                {
+                    if (member.HasAttribute(waypoints.InjectAttribute) is false)
+                        continue;
+
+                    if (method.ReturnsVoid is false) //Only a warning, no need to skip method
+                        context.ReportDiagnostic(LocalDiagnostics.InjectMethodShouldReturnVoid.Create(method));
+
+                    cache.Methods.Add(method);
+                }
+            }
+
+            return cache;
+        }
+
+        /// <summary>
+        /// Validates that a contractor has no source generator required syntax errors, making sure the type alongside it's container hierarchy is both partial and public
+        /// </summary>
+        /// <returns>true if valid, false if not</returns>
+        bool ValidateContractorContainerChain(GeneratorExecutionContext context, INamedTypeSymbol contractor)
+        {
+            foreach (var type in contractor.IterateTypeContainingChain())
+            {
+                if (type.DeclaredAccessibility != Accessibility.Public)
+                {
+                    context.ReportDiagnostic(LocalDiagnostics.ContractorMustBePublic.Create(type));
+                    return false;
+                }
+                if (type.IsPartial() == false)
+                {
+                    context.ReportDiagnostic(LocalDiagnostics.ContractorMustBePartial.Create(type));
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Shortcut for writing "<![CDATA[container.Resolve<type>()]]>"
+        /// </summary>
+        static void WriteContainerResolutionCall(CodeStringBuilder codeBuilder, ITypeSymbol genericType)
+        {
+            codeBuilder.Write("container.Resolve");
+
+            using (codeBuilder.GenericArguments())
+            {
+                codeBuilder.Write(genericType);
+            }
+
+            codeBuilder.Write("()");
+        }
+
+        /// <summary>
+        /// Shortcut for writing "<![CDATA[void IAttributeInjectionContract.method(Container container)]]>"
+        /// </summary>
+        static void StartWriteInterfaceImplementation(CodeStringBuilder builder, string method)
+        {
+            builder.Write("void ");
+            builder.Write(Constants.IAttributeInjectionContract);
+
+            builder.Write(".");
+            builder.Write(method);
+
+            using (builder.Parameters())
+            {
+                builder.Write(Constants.Container);
+                builder.Write(" container");
+            }
+        }
+
+        void WriteSourceFile(GeneratorExecutionContext context, CodeStringBuilder builder)
+        {
+            var hint = $"{context.Compilation.AssemblyName}_SourceGeneratedInjectors.g.cs";
+            var source = builder.ToString();
+
+            context.AddSource(hint, source);
+        }
+
+        public struct WaypointsData
+        {
+            public INamedTypeSymbol InjectAttribute { get; }
+            public INamedTypeSymbol IAttributeInjectionContract { get; }
+            public INamedTypeSymbol SourceGeneratorInjectableAttribute { get; }
+
+            CacheData Cache;
+            public struct CacheData
+            {
+                public List<INamespaceOrTypeSymbol> Hierarchy { get; private set; }
+
+                public List<IFieldSymbol> Fields { get; private set; }
+                public List<IPropertySymbol> Properties { get; private set; }
+                public List<IMethodSymbol> Methods { get; private set; }
+
+                public void Clear()
+                {
+                    Hierarchy.Clear();
+
+                    Fields.Clear();
+                    Properties.Clear();
+                    Methods.Clear();
+                }
+
+                public static CacheData Create() => new CacheData()
+                {
+                    Hierarchy = new List<INamespaceOrTypeSymbol>(),
+
+                    Fields = new List<IFieldSymbol>(),
+                    Properties = new List<IPropertySymbol>(),
+                    Methods = new List<IMethodSymbol>(),
+                };
+            }
+            public CacheData GetCache()
+            {
+                Cache.Clear();
+                return Cache;
+            }
+
+            public WaypointsData(Compilation compilation)
+            {
+                InjectAttribute = compilation.GetTypeByMetadataName(Constants.InjectAttribute);
+                IAttributeInjectionContract = compilation.GetTypeByMetadataName(Constants.IAttributeInjectionContract);
+                SourceGeneratorInjectableAttribute = compilation.GetTypeByMetadataName(Constants.SourceGeneratorInjectableAttribute);
+
+                Cache = CacheData.Create();
+            }
+        }
+
+        public class InjectionVisitor : SymbolVisitor
+        {
+            public List<INamedTypeSymbol> Contractors { get; }
+
+            public override void VisitNamespace(INamespaceSymbol symbol)
+            {
+                foreach (var member in symbol.GetMembers())
+                    Visit(member);
+            }
+            public override void VisitNamedType(INamedTypeSymbol symbol)
+            {
+                foreach (var member in symbol.GetTypeMembers())
+                    Visit(member);
+
+                if (symbol.HasAttribute(Waypoints.SourceGeneratorInjectableAttribute))
+                    Contractors.Add(symbol);
+            }
+
+            readonly WaypointsData Waypoints;
+            public InjectionVisitor(WaypointsData Waypoints)
+            {
+                this.Waypoints = Waypoints;
+                Contractors = new List<INamedTypeSymbol>();
+            }
+
+            public static List<INamedTypeSymbol> Collect(WaypointsData waypoints, ISymbol root)
+            {
+                var visitor = new InjectionVisitor(waypoints);
+
+                root.Accept(visitor);
+
+                return visitor.Contractors;
+            }
+        }
+
+        public static class LocalDiagnostics
+        {
+            public const string ProjectSymbol = "RFX";
+
+            public static DiagnosticDescriptor ContractorMustBePartial =
+                new DiagnosticDescriptor(ProjectSymbol + "1",
+                    "Source generated contractor must be partial",
+                    "Source generated contractors and it's entire containing type chain must be all declared partial",
+                    "Usage",
+                    DiagnosticSeverity.Error,
+                    true);
+
+            public static DiagnosticDescriptor ContractorMustBePublic =
+                new DiagnosticDescriptor(ProjectSymbol + "2",
+                    "Source generated contractor must be public",
+                    "Source generated contractors and it's entire containing type chain must be all declared public",
+                    "Usage",
+                    DiagnosticSeverity.Error,
+                    true);
+
+            public static DiagnosticDescriptor InjectPropertiesMustBeWritable =
+                new DiagnosticDescriptor(ProjectSymbol + "3",
+                    "Source generated injectable properties must be writable",
+                    "Source generated injectable properties must be writable",
+                    "Usage",
+                    DiagnosticSeverity.Error,
+                    true);
+
+            public static DiagnosticDescriptor InjectMethodShouldReturnVoid =
+                new DiagnosticDescriptor(ProjectSymbol + "4",
+                    "Source generated injectable methods should return void",
+                    "Source generated injectable methods should return void, the return value will be discarded",
+                    "Usage",
+                    DiagnosticSeverity.Warning,
+                    true);
+
+            public static DiagnosticDescriptor InjectMemberMustNotBeReadonly =
+                new DiagnosticDescriptor(ProjectSymbol + "5",
+                    "Source generated injectable members can't be readonly",
+                    "Source generated injectable members can't be readonly",
+                    "Usage",
+                    DiagnosticSeverity.Error,
+                    true);
+        }
     }
 }
---
+++ Reflex.Generator/Reflex.Generator.Injector/Tools/SourceGeneratorUtility.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Reflex.Generator.Injector
+{
+    public static class SourceGeneratorUtility
+    {
+        public static SymbolEqualityComparer SymbolEquality => SymbolEqualityComparer.Default;
+
+        public static bool HasAttribute(this ISymbol parameter, INamedTypeSymbol attribute)
+        {
+            var collection = parameter.GetAttributes();
+
+            foreach (var data in collection)
+                if (SymbolEquality.Equals(attribute, data.AttributeClass))
+                    return true;
+
+            return false;
+        }
+
+        public static bool IsPartial(this ITypeSymbol symbol, CancellationToken cancellation = default)
+        {
+            if (symbol.DeclaringSyntaxReferences.Length is 0)
+                return false;
+
+            var declaration = symbol.DeclaringSyntaxReferences[0].GetSyntax(cancellation) as MemberDeclarationSyntax;
+            if (declaration is null)
+                return false;
+
+            foreach (var modifier in declaration.Modifiers)
+                if (modifier.IsKind(SyntaxKind.PartialKeyword))
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Iterates the type's containing type chain, starting from symbol to it's container and so on
+        /// </summary>
+        public static IEnumerable<INamedTypeSymbol> IterateTypeContainingChain(this INamedTypeSymbol symbol)
+        {
+            while (symbol != null)
+            {
+                yield return symbol;
+                symbol = symbol.ContainingType;
+            }
+        }
+
+        /// <summary>
+        /// Iterates the type's containing namespace and type chain, starting from symbol to it's container and so on
+        /// </summary>
+        public static IEnumerable<INamespaceOrTypeSymbol> IterateNamespaceOrTypeContainingChain(this INamespaceOrTypeSymbol symbol)
+        {
+            while (symbol != null)
+            {
+                yield return symbol;
+                symbol = symbol.ContainingSymbol as INamespaceOrTypeSymbol;
+
+                if (symbol is INamespaceSymbol @namespace && @namespace.IsGlobalNamespace)
+                    yield break;
+            }
+        }
+
+        public static Diagnostic Create(this DiagnosticDescriptor descriptor) => Create(descriptor, Location.None);
+        public static Diagnostic Create(this DiagnosticDescriptor descriptor, ISymbol symbol) => Create(descriptor, symbol.Locations[0]);
+        public static Diagnostic Create(this DiagnosticDescriptor descriptor, Location location)
+        {
+            return Diagnostic.Create(descriptor, location);
+        }
+        public static Diagnostic Create(this DiagnosticDescriptor descriptor, Location location, params object[] arguments)
+        {
+            return Diagnostic.Create(descriptor, location, arguments);
+        }
+    }
+}
