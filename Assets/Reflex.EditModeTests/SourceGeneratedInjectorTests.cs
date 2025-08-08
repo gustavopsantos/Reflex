f9f3d485787e46a0b557ed2e1c6160c328b967ee
Moe-Baker <moe4baker@gmail.com>
2025-08-08 19:21:31 +0300

    Write Unity Tests for Source Generated Injection
    New tests pass as expected.
    Strangely though, 4 old tests fail, I switched branches to the main and ran the same tests still failed, not sure why, maybe because I'm using Unity 6.
    Nonetheless, it doesn't seem to be my fault, but I suppose the pull request will make it clear.

---
+++ Assets/Reflex.EditModeTests/SourceGeneratedInjectorTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Reflex.Attributes;
+using Reflex.Extensions;
+using Reflex.Injectors;
+using Reflex.Core;
+
+namespace Reflex.EditModeTests
+{
+    internal class SourceGeneratorAttributeInjectorTests
+    {
+        [Test]
+        public void MockUsage_ImplementsInterface()
+        {
+            var sample = new MockParent.MockUsage();
+            Assert.IsTrue(sample is Reflex.Injectors.IAttributeInjectionContract);
+        }
+
+        [Test]
+        public void MockUsage_Injection()
+        {
+            var TestDependency = "Hello World";
+
+            var container = new ContainerBuilder()
+                .AddSingleton(TestDependency)
+                .Build();
+
+            var sample = new MockParent.MockUsage();
+
+            AttributeInjector.Inject(sample, container);
+
+            Assert.AreEqual(sample.SampleField, TestDependency);
+            Assert.AreEqual(sample.SampleProperty, TestDependency);
+            Assert.AreEqual(sample.SampleMethodParameter, TestDependency);
+        }
+    }
+
+    public partial class MockParent
+    {
+        [SourceGeneratorInjectable]
+        public partial class MockUsage
+        {
+            [Inject]
+            public string SampleField;
+
+            [Inject]
+            public string SampleProperty { get; private set; }
+
+            public string SampleMethodParameter;
+            [Inject]
+            public void SampleMethod(string SampleMethodParameter)
+            {
+                this.SampleMethodParameter = SampleMethodParameter;
+            }
+        }
+    }
+}
