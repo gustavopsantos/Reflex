0c1e4646fa9a0fa12ba5f29e5693a98eb4fb3f24
Moe-Baker <moe4baker@gmail.com>
2025-08-08 19:11:44 +0300

    Implement Injector Source Generator Test Project

---
+++ Reflex.Generator/Reflex.Generator.Injector.Tests/MockUsageTests.cs
@@ -0,0 +1,36 @@
+﻿using Level1.Level2;
+
+using Reflex.Core;
+using Reflex.Injectors;
+
+namespace Reflex.Generator.Injector.Tests
+{
+    [TestClass]
+    public sealed class MockUsageTests
+    {
+        [TestMethod]
+        public void ImplementsInterface() //Somewhat un-needed since the injection test bellow will cause a compile time exception first, but whatever ¯\_(ツ)_/¯
+        {
+            var sample = new MockContainer.MockUsage();
+            Assert.IsTrue(sample is Reflex.Injectors.IAttributeInjectionContract);
+        }
+
+        [TestMethod]
+        public void Injection()
+        {
+            var TestDependency = "Hello World";
+
+            var container = new Container();
+
+            container.AddSingleton(TestDependency);
+
+            var sample = new MockContainer.MockUsage();
+
+            AttributeInjector.Inject(sample, container);
+
+            Assert.AreEqual(sample.SampleField, TestDependency);
+            Assert.AreEqual(sample.SampleProperty, TestDependency);
+            Assert.AreEqual(sample.SampleMethodParameter, TestDependency);
+        }
+    }
+}
