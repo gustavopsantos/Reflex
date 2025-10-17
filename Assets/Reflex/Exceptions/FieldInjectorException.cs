e9712e3b9c7593a7595b4a57b84d499c3368ed2d
Gustavo Santos <gustappsantos@gmail.com>
2025-10-17 10:12:29 -0300

    Fix missing inner exception on Field/Property/Method InjectorException

--- Assets/Reflex/Exceptions/FieldInjectorException.cs
+++ Assets/Reflex/Exceptions/FieldInjectorException.cs
@@ -5,14 +5,14 @@ namespace Reflex.Exceptions
 {
     internal sealed class FieldInjectorException : Exception
     {
-        public FieldInjectorException(FieldInfo field, Exception e) : base(BuildMessage(field, e))
+        public FieldInjectorException(FieldInfo field, Exception innerException) : base(BuildMessage(field, innerException), innerException)
         {
         }
         
-        private static string BuildMessage(FieldInfo field, Exception e)
+        private static string BuildMessage(FieldInfo field, Exception innerException)
         {
             var fieldDescription = $"'{field.DeclaringType.Name}.{field.Name}'";
-            return $"Could not inject field {fieldDescription}, exception: {e}";
+            return $"Could not inject field {fieldDescription}, inner exception: {innerException}";
         }
     }
 }
