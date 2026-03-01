9533f95240d6d717bf441e3aaeb67da04ea3b67c
octia <74676459+octia@users.noreply.github.com>
2026-03-01 01:03:12 +0100

    fixes

--- Assets/Reflex/Samples/OldAPICompatibility/OldAPICompatibility.cs
+++ Assets/Reflex/Samples/OldAPICompatibility/OldAPICompatibility.cs
@@ -5,49 +5,49 @@ namespace Reflex.Compatibility.OldAPI
 {
     public static class OldAPICompatibility
     {
-        public static void AddSingleton(this ContainerBuilder builder, Type concrete, params Type[] contracts)
+        public static ContainerBuilder AddSingleton(this ContainerBuilder builder, Type concrete, params Type[] contracts)
         {
-            builder.RegisterType(concrete, contracts, Enums.Lifetime.Singleton, Enums.Resolution.Lazy);
+            return builder.RegisterType(concrete, contracts, Enums.Lifetime.Singleton, Enums.Resolution.Lazy);
         }
 
-        public static void AddSingleton(this ContainerBuilder builder, object instance, params Type[] contracts)
+        public static ContainerBuilder AddSingleton(this ContainerBuilder builder, object instance, params Type[] contracts)
         {
-            builder.RegisterValue(instance, contracts);
+            return builder.RegisterValue(instance, contracts);
         }
 
-        public static void AddSingleton<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
+        public static ContainerBuilder AddSingleton<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
         {
-            builder.RegisterFactory(factory, contracts, Enums.Lifetime.Singleton, Enums.Resolution.Lazy);
+            return builder.RegisterFactory(factory, contracts, Enums.Lifetime.Singleton, Enums.Resolution.Lazy);
         }
 
-        public static void AddTransient(this ContainerBuilder builder, Type concrete, params Type[] contracts)
+        public static ContainerBuilder AddTransient(this ContainerBuilder builder, Type concrete, params Type[] contracts)
         {
-            builder.RegisterType(concrete, contracts, Enums.Lifetime.Transient, Enums.Resolution.Lazy);
+            return builder.RegisterType(concrete, contracts, Enums.Lifetime.Transient, Enums.Resolution.Lazy);
         }
 
-        public static void AddTransient(this ContainerBuilder builder, object instance, params Type[] contracts)
+        public static ContainerBuilder AddTransient(this ContainerBuilder builder, object instance, params Type[] contracts)
         {
-            builder.RegisterValue(instance, contracts);
+            return builder.RegisterValue(instance, contracts);
         }
 
-        public static void AddTransient<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
+        public static ContainerBuilder AddTransient<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
         {
-            builder.RegisterFactory(factory, contracts, Enums.Lifetime.Transient, Enums.Resolution.Lazy);
+            return builder.RegisterFactory(factory, contracts, Enums.Lifetime.Transient, Enums.Resolution.Lazy);
         }
 
-        public static void AddScoped(this ContainerBuilder builder, Type concrete, params Type[] contracts)
+        public static ContainerBuilder AddScoped(this ContainerBuilder builder, Type concrete, params Type[] contracts)
         {
-            builder.RegisterType(concrete, contracts, Enums.Lifetime.Scoped, Enums.Resolution.Lazy);
+            return builder.RegisterType(concrete, contracts, Enums.Lifetime.Scoped, Enums.Resolution.Lazy);
         }
 
-        public static void AddScoped(this ContainerBuilder builder, object instance, params Type[] contracts)
+        public static ContainerBuilder AddScoped(this ContainerBuilder builder, object instance, params Type[] contracts)
         {
-            builder.RegisterValue(instance, contracts);
+            return builder.RegisterValue(instance, contracts);
         }
 
-        public static void AddScoped<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
+        public static ContainerBuilder AddScoped<T>(this ContainerBuilder builder, Func<Container, T> factory, params Type[] contracts)
         {
-            builder.RegisterFactory(factory, contracts, Enums.Lifetime.Scoped, Enums.Resolution.Lazy);
+            return builder.RegisterFactory(factory, contracts, Enums.Lifetime.Scoped, Enums.Resolution.Lazy);
         }
     }
 }
