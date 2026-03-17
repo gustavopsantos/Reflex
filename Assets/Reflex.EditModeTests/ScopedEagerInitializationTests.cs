8ca2f8aa9f51da8e5f9d2ee632b9556436d7fe08
Gustavo Santos <gustappsantos@gmail.com>
2026-03-17 11:12:23 -0300

    update ScopedEagerInitializationTests

--- Assets/Reflex.EditModeTests/ScopedEagerInitializationTests.cs
+++ Assets/Reflex.EditModeTests/ScopedEagerInitializationTests.cs
@@ -1,7 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
 using Reflex.Core;
 using Reflex.Enums;
-using UnityEngine;
 using Resolution = Reflex.Enums.Resolution;
 
 namespace Reflex.EditModeTests
@@ -10,32 +11,30 @@ namespace Reflex.EditModeTests
     {
         class ScopedService
         {
+            public static List<ScopedService> Instances { get; } = new(); 
+            
             public ScopedService()
             {
-                Debug.Log($"ScopedService {GetHashCode()} created.");
+                Instances.Add(this);
             }
         }
-        
-        [Test]
-        public void Foo()
-        {
-            var containerBuilder = new ContainerBuilder()
-                .RegisterType(typeof(ScopedService), Lifetime.Scoped, Resolution.Lazy);
 
-            // containerBuilder.OnContainerBuilt += OnContainerBuilt;
-            //
-            // void OnContainerBuilt(Container c)
-            // {
-            //     containerBuilder.OnContainerBuilt -= OnContainerBuilt;
-            //     c.Resolve<ScopedService>();
-            //     c.OnContainerScoped += OnContainerBuilt;
-            // }
+        [Test]
+        public void EnsureScopedEagerInstancesAreConstructedAsSoonAsContainerIsBuilt()
+        {
+            ScopedService.Instances.Clear();
             
-            var container = containerBuilder.Build();
-            var scoped1 = container.Scope();
-            var scoped2 = container.Scope();
+            var builder = new ContainerBuilder().RegisterType(typeof(ScopedService), Lifetime.Scoped, Resolution.Eager);
+            ScopedService.Instances.Count.Should().Be(0);
+            
+            var containerA = builder.Build();
+            ScopedService.Instances.Count.Should().Be(1);
+            
+            var scoped0 = containerA.Scope();
+            var scoped1 = containerA.Scope();
+            var scoped2 = scoped1.Scope();
             var scoped3 = scoped2.Scope();
-            var scoped4 = scoped3.Scope();
+            ScopedService.Instances.Count.Should().Be(5);
         }
     }
 }
